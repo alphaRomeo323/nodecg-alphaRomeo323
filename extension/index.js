9316f1312ed15bb50f249aec9297527a14383132
alphaRomeo323 <alpharomeo323dev@protonmail.com>
2023-12-11 23:16:10 +0900

    Removed mopidy

--- extension/index.js
+++ extension/index.js
@@ -1,11 +1,8 @@
-const Mopidy = require("mopidy")
-const mopidyModule = require("./mopidy")
 const send = require("./info_send");
 
 module.exports = function (nodecg) {
     const playBackRep = nodecg.Replicant("playback");
     const activeRep = nodecg.Replicant("mopidy-active");
-    const logRep = nodecg.Replicant("mopidy-connection-log");
     const guestsRep = nodecg.Replicant("guests");
 
     //Initialize
@@ -13,25 +10,7 @@ module.exports = function (nodecg) {
         guestsRep.value = [];
     playBackRep.value = { playing: false };
     activeRep.value = false;
-    logRep.value = "Closed..."
 
-    let mopidy;
-
-    //mopidy websocket
-    activeRep.on("change", (newValue) => {
-        if (newValue === true){
-            if (typeof mopidy === "object"){
-                mopidy.close()
-            }
-            logRep.value = "Added New Client..."
-            mopidy = new Mopidy({
-                webSocketUrl: nodecg.Replicant("mopidy-server").value,    
-            })
-            nodecg.log.info("Added new Mopidy client.")
-            mopidyModule( mopidy, nodecg )
-        }
-    })
-    
     //callback of replicants
     nodecg.Replicant("description").on("change", ()=>{
         send(nodecg);
@@ -40,6 +19,12 @@ module.exports = function (nodecg) {
         send(nodecg);
     })
     nodecg.Replicant("misskey-acct").on("change", ()=>{
+        send(nodecg);
+    })
+    nodecg.Replicant("youtube-handle").on("change", ()=>{
+        send(nodecg);
+    })
+    nodecg.Replicant("twitch-id").on("change", ()=>{
         send(nodecg);
     })
     nodecg.Replicant("guests").on("change", () => {
